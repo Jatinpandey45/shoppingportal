e828d71d8b03cf2b94e8b18b62d21fe11306d068
Appinventiv <satish15625@gmail>
2019-08-12 23:39:13 +0530

    update

---
+++ application/models/User_model.php
@@ -0,0 +1,41 @@
+<?php 
+
+class User_model extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        
+    }
+
+    /**
+     * function : getUser
+     * @param : email
+     * @param : password
+     */
+
+     public function getUser($userEmail,$password) 
+     {  
+        
+         $this->db->select('auth_user.id,auth_user.email,auth_user.username,
+                            auth_user.first_name,auth_user.last_name')
+                            
+                            ->from('auth_user')
+
+                            ->join('vg_user_profile','vg_user_profile.user_id = auth_user.id','INNER')
+
+                            ->where('auth_user.email',$userEmail)
+
+                            ->where('vg_user_profile.password',$password);
+
+        $query = $this->db->get();
+       
+        return $query->row_array();
+         
+     }
+
+
+
+
+}
