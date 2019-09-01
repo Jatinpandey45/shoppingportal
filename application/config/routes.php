b3d33da72cd32cde6f12f830a7db7d738a35850d
Appinventiv <satish15625@gmail>
2019-09-01 22:35:45 +0530

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'shop/home';
 $route['store'] = "store/home";
+$route['vcmadmin'] = "vcmadmin/home";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
